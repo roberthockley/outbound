ebdfa671e7b13adcb4fa20228d6588cb38af091b
Robert Hockley <robert.hockley@accenture.com>
2025-01-23 19:26:17 +0800

    API

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -123,7 +123,7 @@ resource "aws_api_gateway_integration_response" "connect_outbound_options" {
 }
 
 resource "aws_api_gateway_integration_response" "connect_outbound_post" {
-  depends_on  = [aws_api_gateway_method.connect_outbound_post]
+  depends_on  = [aws_api_gateway_method.connect_outbound_post, aws_api_gateway_integration.connect_outbound_scan]
   http_method = "POST"
   resource_id = aws_api_gateway_resource.connect_outbound.id
   response_parameters = {
