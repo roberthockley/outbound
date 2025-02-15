8bfa3890d75b81599e909d30cd0194208d4c7b28
Robert Hockley <robert.hockley@accenture.com>
2025-02-15 16:39:29 +0800

    Test

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -16,6 +16,29 @@ resource "aws_iam_role" "RoleForDynamoDB" {
   name        = "RoleForDynamoDB"
 }
 
+resource "aws_iam_policy" "RoleForDynamoDBIAM" {
+  depends_on  = [aws_kms_key.kms_ihs] #### Ensure this is happening first
+  name        = "${var.project.tla}-${var.environment.name}-secrets_rotation"
+  path        = "/"
+  description = "For the Secret Rotation Lambda"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Sid" : "VisualEditor0",
+        "Effect" : "Allow",
+        "Action" : [
+          "iam:PassRole"
+        ],
+        "Resource" : [
+          "arn:aws:iam::117134819170:role/Events",
+        ]
+      }
+    ]
+  })
+}
+
+
 resource "aws_iam_role_policy_attachment" "RoleForDynamoDB_AmazonDynamoDBFullAccess" {
   depends_on = [aws_iam_role.RoleForDynamoDB]
   policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
