d9bfb8604b89c9287fedc09221b4ef614925b023
Robert Hockley <robert.hockley@accenture.com>
2025-03-17 10:11:13 +0800

    Contact Lens

--- terraform/connect_instance.tf
+++ terraform/connect_instance.tf
@@ -3,7 +3,7 @@ resource "aws_connect_instance" "song" {
   inbound_calls_enabled            = true
   instance_alias                   = "song-connect"
   outbound_calls_enabled           = true
-  contact_lens_enabled             = false
+  contact_lens_enabled             = true
   contact_flow_logs_enabled        = true
   auto_resolve_best_voices_enabled = true
 }
