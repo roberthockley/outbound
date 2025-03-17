c9dc692aa178721b9d92b2f2d62df6dc7fb33fb7
Robert Hockley <robert.hockley@accenture.com>
2025-03-17 14:30:07 +0800

    DNC

--- src/components/SiteSettings.js
+++ src/components/SiteSettings.js
@@ -6,7 +6,6 @@ import { Input } from '@twilio-paste/core/input';
 import { Heading } from '@twilio-paste/core/heading';
 import { Button } from '@twilio-paste/core/button';
 import { Table, THead, Tr, Td, Th, TBody, TFoot } from '@twilio-paste/core/table';
-import { TimePicker } from '@twilio-paste/core/time-picker';
 import { Checkbox, HelpText, Label } from '@twilio-paste/core';
 import { DatePicker, formatReturnDate } from '@twilio-paste/core/date-picker';
 import { Modal, ModalBody, ModalFooter, ModalFooterActions, ModalHeader, ModalHeading } from '@twilio-paste/core/modal';
@@ -49,66 +48,9 @@ export const SiteSettings = () => {
     const [start, setStart] = React.useState('');
     const [end, setEnd] = React.useState('');
     const [updateCamapign, setUpdateCampaign] = React.useState(true);
-    const [showTimes, setShowTimes] = React.useState(false);
     const [selectedCampaign, setSelectedCampaign] = React.useState(false);
     const [multiTimes, setMultiTimes] = React.useState(false);
-    const [mondayStart1, setMondayStart1] = React.useState("09:00:00");
-    const [mondayEnd1, setMondayEnd1] = React.useState("12:00:00");
-    const [mondayChecked, setMondayChecked] = React.useState(false);
-    const [mondayHelp1, setMondayHelp1] = React.useState(false);
-    const [tuesdayStart1, setTuesdayStart1] = React.useState("09:00:00");
-    const [tuesdayEnd1, setTuesdayEnd1] = React.useState("12:00:00");
-    const [tuesdayChecked, setTuesdayChecked] = React.useState(false);
-    const [tuesdayHelp1, setTuesdayHelp1] = React.useState(false);
-    const [wednesdayStart1, setWednesdayStart1] = React.useState("09:00:00");
-    const [wednesdayEnd1, setWednesdayEnd1] = React.useState("12:00:00");
-    const [wednesdayChecked, setWednesdayChecked] = React.useState(false);
-    const [wednesdayHelp1, setWednesdayHelp1] = React.useState(false);
-    const [thursdayStart1, setThursdayStart1] = React.useState("09:00:00");
-    const [thursdayEnd1, setThursdayEnd1] = React.useState("12:00:00");
-    const [thursdayChecked, setThursdayChecked] = React.useState(false);
-    const [thursdayHelp1, setThursdayHelp1] = React.useState(false);
-    const [fridayStart1, setFridayStart1] = React.useState("09:00:00");
-    const [fridayEnd1, setFridayEnd1] = React.useState("12:00:00");
-    const [fridayChecked, setFridayChecked] = React.useState(false);
-    const [fridayHelp1, setFridayHelp1] = React.useState(false);
-    const [saturdayStart1, setSaturdayStart1] = React.useState("09:00:00");
-    const [saturdayEnd1, setSaturdayEnd1] = React.useState("12:00:00");
-    const [saturdayChecked, setSaturdayChecked] = React.useState(false);
-    const [saturdayHelp1, setSaturdayHelp1] = React.useState(false);
-    const [sundayStart1, setSundayStart1] = React.useState("09:00:00");
-    const [sundayEnd1, setSundayEnd1] = React.useState("12:00:00");
-    const [sundayChecked, setSundayChecked] = React.useState(false);
-    const [sundayHelp1, setSundayHelp1] = React.useState(false);
-    const [mondayStart2, setMondayStart2] = React.useState("23:58:00");
-    const [mondayEnd2, setMondayEnd2] = React.useState("23:59:00");
-    const [mondayHelp2, setMondayHelp2] = React.useState(false);
-    const [tuesdayStart2, setTuesdayStart2] = React.useState("23:58:00");
-    const [tuesdayEnd2, setTuesdayEnd2] = React.useState("23:59:00");
-    const [tuesdayHelp2, setTuesdayHelp2] = React.useState(false);
-    const [wednesdayStart2, setWednesdayStart2] = React.useState("23:58:00");
-    const [wednesdayEnd2, setWednesdayEnd2] = React.useState("23:59:00");
-    const [wednesdayHelp2, setWednesdayHelp2] = React.useState(false);
-    const [thursdayStart2, setThursdayStart2] = React.useState("23:58:00");
-    const [thursdayEnd2, setThursdayEnd2] = React.useState("23:59:00");
-    const [thursdayHelp2, setThursdayHelp2] = React.useState(false);
-    const [fridayStart2, setFridayStart2] = React.useState("23:58:00");
-    const [fridayEnd2, setFridayEnd2] = React.useState("23:59:00");
-    const [fridayHelp2, setFridayHelp2] = React.useState(false);
-    const [saturdayStart2, setSaturdayStart2] = React.useState("23:58:00");
-    const [saturdayEnd2, setSaturdayEnd2] = React.useState("23:59:00");
-    const [saturdayHelp2, setSaturdayHelp2] = React.useState(false);
-    const [sundayStart2, setSundayStart2] = React.useState("23:58:00");
-    const [sundayEnd2, setSundayEnd2] = React.useState("23:59:00");
-    const [sundayHelp2, setSundayHelp2] = React.useState(false);
-    const [mondayHelp3, setMondayHelp3] = React.useState(false);
-    const [tuesdayHelp3, setTuesdayHelp3] = React.useState(false);
-    const [wednesdayHelp3, setWednesdayHelp3] = React.useState(false);
-    const [thursdayHelp3, setThursdayHelp3] = React.useState(false);
-    const [fridayHelp3, setFridayHelp3] = React.useState(false);
-    const [saturdayHelp3, setSaturdayHelp3] = React.useState(false);
-    const [sundayHelp3, setSundayHelp3] = React.useState(false);
-    const [dynamoCampaigns, setDynamoCampaigns] = React.useState("");
+    const [dynamoCampaigns, setDynamoCampaigns] = React.useState([]);
     const openModal = () => setModalOpen(true);
     const closeModal = () => setModalOpen(false)
     const [isModalOpen, setModalOpen] = React.useState(false);
@@ -249,7 +191,6 @@ export const SiteSettings = () => {
                 setIsOpen(false)
                 setSiteCampaign(listName);
                 setSelectedCampaign(true);
-                setShowTimes(true);
                 setUpdateCampaign(false);
             }
         }
@@ -291,13 +232,13 @@ export const SiteSettings = () => {
 
     const removeCampaign = () => {
         console.log(siteCampaign)
-        if(siteCampaign.length === 0){
+        /*if(siteCampaign.length === 0){
             toaster.push({
                 message: 'Select List First',
                 variant: 'error',
                 dismissAfter: 3000
             })
-        }
+        }*/
         let campaignToUpdate = []
         let newCampaign = []
         for (let i = 0; i < dynamoCampaigns.length; i++) {
@@ -312,7 +253,6 @@ export const SiteSettings = () => {
         setCampaign(newCampaign);
         setSiteCampaign([])
         setSelectedCampaign(false)
-        setShowTimes(false)
         setDynamoCampaigns(campaignToUpdate)
         localStorage.setItem('campaigns', JSON.stringify(campaignToUpdate));
     }
@@ -334,12 +274,12 @@ export const SiteSettings = () => {
                 for (let i = 0; i < items.length; i++) {
                     currentCampaigns.push(items[i].campaign)
                 }
+                setCampaign(currentCampaigns);
+                console.log(`Campaign(s) are: ${currentCampaigns}`)
             })
             .catch((error) => {
                 console.log(error);
             });
-        setCampaign(currentCampaigns);
-        console.log(`Campaign(s) are: ${currentCampaigns}`)
     }, []);
     
     return (
@@ -360,23 +300,21 @@ export const SiteSettings = () => {
                                     setSiteCampaign(inputValue);
                                     setSelectedCampaign(true);
                                     for (let i = 0; i < dynamoCampaigns.length; i++) {
+
                                         if (dynamoCampaigns[i].campaign === inputValue) {
                                             console.log(dynamoCampaigns[i])
                                             setRetries(dynamoCampaigns[i].retry)
                                             setRetriesWeek(dynamoCampaigns[i].retriesWeek)
                                             setIntervalBusy(dynamoCampaigns[i].intervalBusy)
                                             setDuration(dynamoCampaigns[i].duration)
-                                            setMultiTimes(dynamoCampaigns[i].shift)
                                             setCallerId(dynamoCampaigns[i].callerId)
                                             setDND(dynamoCampaigns[i].dnd)
                                             setStart(dynamoCampaigns[i].start)
                                             setEnd(dynamoCampaigns[i].end)
                                             setEnabled(dynamoCampaigns[i].enabled)
                                             setTZ(dynamoCampaigns[i].timezone)
-                                            console.log(multiTimes, dynamoCampaigns[i].shift)
                                         }
                                     }
-
                                 }}
                             />
                         </td>
@@ -511,22 +449,6 @@ export const SiteSettings = () => {
                         /></Td>
                     </Tr>
                     <Tr>
-                        <Td>Mulriple Schedules</Td>
-                        <Td></Td>
-                        <Td></Td>
-                        <Td><Combobox
-                            items={["True", "False"]}
-                            value={multiTimes}
-                            onInputValueChange={({ inputValue }) => {
-                                if (inputValue === "True") {
-                                    setMultiTimes(true)
-                                } else {
-                                    setMultiTimes(false)
-                                }
-                            }}
-                        /></Td>
-                    </Tr>
-                    <Tr>
                         <Td>Caller Id</Td>
                         <Td></Td>
                         <Td></Td>
@@ -543,24 +465,12 @@ export const SiteSettings = () => {
                         <Td></Td>
                         <Td></Td>
                         <Td><Combobox
-                            //items={["Global", "Sales", "Collections"]}
+                            items={["Global", "Sales", "Collections"]}
                             value={dnd}
                             onInputValueChange={({ inputValue }) => {
                                 setDND(inputValue)
                             }}
                         /></Td>
-                    </Tr>
-                    <Tr>
-                        <Td>Time Zone</Td>
-                        <Td></Td>
-                        <Td></Td>
-                        <Td><Combobox
-                            items={timezones}
-                            value={tz}
-                            onInputValueChange={({ inputValue }) => {
-                                setTZ(inputValue)
-                            }}/>
-                        </Td>
                     </Tr>
                     <Tr>
                         <Td>Start Date</Td>
