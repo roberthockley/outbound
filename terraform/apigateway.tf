e2965377186f1576b6702325f6eca4b83227b08b
Robert Hockley <robert.hockley@accenture.com>
2025-03-18 12:20:28 +0800

    API

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -37,7 +37,7 @@ resource "aws_api_gateway_resource" "connect_outbound_make" {
 
 resource "aws_api_gateway_resource" "connect_outbound_numbers" {
   parent_id   = aws_api_gateway_rest_api.connect_outbound.root_resource_id
-  path_part   = "getNumbers2"
+  path_part   = "getNumbers"
   rest_api_id = aws_api_gateway_rest_api.connect_outbound.id
 }
 
