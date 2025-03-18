67f5d69b93adee58c8f53f2a7d9285037b2f0735
Robert Hockley <robert.hockley@accenture.com>
2025-03-18 16:28:29 +0800

    API

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -485,7 +485,7 @@ resource "aws_api_gateway_integration" "connect_outbound_delete_post" {
   rest_api_id             = aws_api_gateway_rest_api.connect_outbound.id
   type                    = "AWS"
   integration_http_method = "POST"
-  uri                     = "arn:aws:apigateway:${var.environment.region}:dynamodb:action/PutItem"
+  uri                     = "arn:aws:apigateway:${var.environment.region}:dynamodb:action/DeleteItem"
   credentials             = aws_iam_role.RoleForMakeCampaign.arn
   passthrough_behavior    = "WHEN_NO_MATCH"
   timeout_milliseconds    = 29000
